5c745245018329c8d2c60e442fe1ac37496b7ec4
SamuelSlavka <slavka.samuel@gmail.com>
2022-03-25 20:44:58 +0100

    small fixes

--- ZkWallet/src/App.tsx
+++ ZkWallet/src/App.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {Provider} from 'react-redux';
-import RootNavigator from './src/app/navigation/RootNavigator';
+import RootNavigator from './app/navigation/RootNavigator';
 import {PersistGate} from 'redux-persist/integration/react';
 
-import {store, persistor} from './src/app/redux/store';
+import {store, persistor} from './app/redux/store';
 
 export default function App() {
   return (
